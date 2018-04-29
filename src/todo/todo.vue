c07d67416b4afac3e7c0e3a9a86ab31560b0f9c6
CinGing <chenguo307@163.com>
2018-04-29 23:54:29 +0800

    add some todos

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -7,7 +7,11 @@
       placeholder="接下来做什么？"
       @keyup.enter="addTodo"
       >
-      <item :todo="todo"></item>
+      <item 
+      :todo="todo"
+      v-for = 'todo in todos'
+      :key="todo.id"
+      />
       <tabs :filter="filter"></tabs>
   </section>
 </template>
@@ -15,14 +19,11 @@
 <script>
 import Item from "./item.vue";
 import Tabs from "./tabs.vue";
+let id = 0;
 export default {
   data() {
     return {
-      todo: {
-        id: 0,
-        content: "this is todo",
-        completed: false
-      },
+      todos: [],
       filter: "all"
     };
   },
@@ -32,34 +33,40 @@ export default {
   },
 
   methods: {
-    addTodo() {}
+    addTodo(e) {
+      this.todos.unshift({
+        id: id++,
+        content: e.target.value.trim(),
+        completed: false
+      });
+    }
   }
 };
 </script>
 
 <style lang="stylus" scoped>
 .real-app {
-    width: 600px;
-    margin: 0 auto;
-    box-shadow: 0 0 5px #666;
+  width: 600px;
+  margin: 0 auto;
+  box-shadow: 0 0 5px #666;
 }
 
 .add-input {
-    position: relative;
-    margin: 0;
-    width: 100%;
-    font-size: 24px;
-    font-family: inherit;
-    font-weight: inherit;
-    line-height: 1.4em;
-    border: 0;
-    outline: none;
-    color: inherit;
-    box-sizing: border-box;
-    font-smoothing: antialiased;
-    padding: 16px 16px 16px 36px;
-    border: none;
-    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+  position: relative;
+  margin: 0;
+  width: 100%;
+  font-size: 24px;
+  font-family: inherit;
+  font-weight: inherit;
+  line-height: 1.4em;
+  border: 0;
+  outline: none;
+  color: inherit;
+  box-sizing: border-box;
+  font-smoothing: antialiased;
+  padding: 16px 16px 16px 36px;
+  border: none;
+  box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
 }
 </style>
 
